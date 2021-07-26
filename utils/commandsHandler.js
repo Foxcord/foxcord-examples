ac0e8cb417f7eb5f8716f6eac07199235fafc839
Lucifer_ <82797989+LuciferHasBeenSpotted@users.noreply.github.com>
2021-07-26 12:12:55 +0200

    Delete utils directory

--- utils/commandsHandler.js
+++
@@ -1,9 +0,0 @@
-import handleFunction from './handleFunction.js'
-
-export default async function(client) {
-  await handleFunction('commands', async path => {
-    const commandeName = path.split('/').pop().split('.')[0],
-          commandCode = await import(path);
-    client.commands.set(commandName, commandCode.default);
-  });
-};
--- utils/eventsHandler.js
+++
@@ -1,9 +0,0 @@
-import handleFunction from './handleFunction.js';
-
-export default async function(client) {
-  await handleFunction('events', async path => {
-    const eventName = path.split('/').pop().split('.')[0].toUpperCase(),
-          eventCode = await import(path);
-    client.on(eventName, eventCode.default.bind(client));
-  });
-};
