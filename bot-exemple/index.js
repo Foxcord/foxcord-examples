4b94c6ea47b3b8cde8c687dc8724124a481bf23f
Lucifer_ <82797989+LuciferHasBeenSpotted@users.noreply.github.com>
2021-07-26 12:20:23 +0200

    Update index.js

--- bot-exemple/index.js
+++ bot-exemple/index.js
@@ -1,1 +1,17 @@
-import { Client, Collection } from 'foxcord'
+import { Client, Collection } from 'foxcord';
+import { TOKEN } from './utils/config.js';
+import commandsLoader from './utils/loaders/commandsLoader.js';
+import eventsLoader from './utils/loaders/eventsLoader.js';
+
+const CLIENT = new Client({
+  device: 'MOBILE',
+  intents: ['ALL'],
+  reconnect: true
+});
+
+['commands', 'cooldowns'].forEach(e => CLIENT[e] = new Collection());
+
+commandsLoader(CLIENT);
+eventsLoader(CLIENT);
+
+CLIENT.connect(TOKEN);
