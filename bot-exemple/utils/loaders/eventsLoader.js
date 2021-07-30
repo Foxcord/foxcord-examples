16fb303538720c3cdd5b4065c6aaf9909c18dbbb
RemyK <48729866+RemyK888@users.noreply.github.com>
2021-07-30 10:39:23 +0200

    Delete eventsLoader.js

--- bot-exemple/utils/loaders/eventsLoader.js
+++
@@ -1,9 +0,0 @@
-import handle from './handle';
-
-export default async function(client) {
-  await handle('events', async path => {
-    const eventName = path.split('/').pop().split('.')[0].toUpperCase(),
-          eventCode = await import(path);
-    client.on(eventName, eventCode.default.bind(client));
-  });
-};
