c8d1fbd0f1b052c5e2b1ebe0e7e01196e2e03b28
RemyK <48729866+RemyK888@users.noreply.github.com>
2021-07-30 10:39:20 +0200

    Delete commandsLoader.js

--- bot-exemple/utils/loaders/commandsLoader.js
+++
@@ -1,9 +0,0 @@
-import handle from './handle.js';
-
-export default async function(client) {
-  await handle('commands', async path => {
-    const commandName = path.split('/').pop().split('.')[0],
-          commandCode = await import(path);
-    client.commands.set(commandName, commandCode.default);
-  });
-};
