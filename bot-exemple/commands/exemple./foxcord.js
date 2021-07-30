15f7ec510b731edb25ec20eba69db04a8faef3c5
Lucifer_ <82797989+LuciferHasBeenSpotted@users.noreply.github.com>
2021-07-30 08:16:25 +0200

    Update foxcord.js

--- bot-exemple/commands/exemple./foxcord.js
+++ bot-exemple/commands/exemple./foxcord.js
@@ -3,7 +3,7 @@ import { DiscordEmbed } from 'foxcord';
 export default {
   //Define Parameters
   
-  args: false, //If the command need arguments
+  args: false, //If the command needs arguments
   dm: true, //If the command can be run in dm
   admin: false, //If the author need admin perms
   async run(message, args) {
