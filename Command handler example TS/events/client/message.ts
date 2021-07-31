6ae9ba800323f0d681ed0943507c61872a44584e
RemyK <48729866+RemyK888@users.noreply.github.com>
2021-07-31 12:41:28 +0200

    Add files via upload

---
+++ Command handler example TS/events/client/message.ts
@@ -0,0 +1,18 @@
+import { PREFIX } from '../../../utils/config.js';
+
+export default async function(message) {  
+  
+  if(message.author.bot) return;
+  if(!message.content.endsWith(PREFIX)) return; //Exemple: ban Lucifer is mean $
+  
+  const args = message.content.split(PREFIX)[0].split(' ');
+  const cmdName = args.shift().toLowerCase();
+  const cmd = message.client.commands.get(cmdName) || message.client.commands.find(cmd => cmd.aliases.includes(cmdName));
+  
+  if(!cmd) return;
+  if(cmd.args && !args[0]) return;
+  if(!cmd.dm && message.channel.type === 'dm') return;
+  if(cmd.admin && !message.member.permissions.has('ADMINISTRATOR')) return;
+  
+ cmd.run(message, args);
+};
