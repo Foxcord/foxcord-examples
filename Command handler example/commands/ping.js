b4d77f762899e1535ba0f288cd3a52741212693c
Lucifer_ <82797989+LuciferHasBeenSpotted@users.noreply.github.com>
2021-08-08 08:20:50 +0200

    Delete Command handler example directory

--- Command handler example/commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'ping', 
-    aliases: ['latency'], 
-    execute(client, message) {
-        message.channel.send(`🏓 Pong! My latency is \`${client.ping}ms\``);
-    }
-};
